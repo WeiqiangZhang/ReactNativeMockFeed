c7d2f983f045d9c983795626cf9b944b1e1774cb
Weiqiang Zhang <weiqiang.zhang@mail.utoronto.ca>
2020-11-11 21:49:34 -0500

    added some error checking

--- src/components/FeedInfo/FeedInfo.js
+++ src/components/FeedInfo/FeedInfo.js
@@ -10,35 +10,43 @@ export default class FeedInfo extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.user}>
+          {post.image.profile && (
+            <Image
+              style={styles.profileImage}
+              source={{
+                uri: post.image.profile,
+              }}
+            />
+          )}
+          {post.username && (
+            <Text style={styles.username}>{post.username}</Text>
+          )}
+        </View>
+        {post.image.post && (
           <Image
-            style={styles.profileImage}
+            style={styles.image}
             source={{
-              uri: post.image.profile,
+              uri: post.image.post,
             }}
           />
-          <Text style={styles.username}>{post.username}</Text>
-        </View>
-        <Image
-          style={styles.image}
-          source={{
-            uri: post.image.post,
-          }}
-        />
-        <Text style={styles.caption}>{post.caption}</Text>
-        <View style={styles.statsContainer}>
-          <View style={styles.stats}>
-            <Icon style={styles.icon} name="star" />
-            <Text>{post.stats.star}</Text>
+        )}
+        {post.caption && <Text style={styles.caption}>{post.caption}</Text>}
+        {post.stats && (
+          <View style={styles.statsContainer}>
+            <View style={styles.stats}>
+              <Icon style={styles.icon} name="star" />
+              <Text>{post.stats.star}</Text>
+            </View>
+            <View style={styles.stats}>
+              <Icon style={styles.icon} name="md-people" />
+              <Text>{post.stats.follow}</Text>
+            </View>
+            <View style={styles.stats}>
+              <Icon style={styles.icon} name="md-eye" />
+              <Text>{post.stats.views}</Text>
+            </View>
           </View>
-          <View style={styles.stats}>
-            <Icon style={styles.icon} name="md-people" />
-            <Text>{post.stats.follow}</Text>
-          </View>
-          <View style={styles.stats}>
-            <Icon style={styles.icon} name="md-eye" />
-            <Text>{post.stats.views}</Text>
-          </View>
-        </View>
+        )}
       </View>
     );
   }
